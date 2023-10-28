4484d9a303926efa19e354787d0894270c073eea
rootnure <nur.diu.2791@gmail.com>
2023-10-28 22:58:49 +0600

    DELETE added for single booking

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookingsCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
